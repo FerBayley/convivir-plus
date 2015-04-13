62aaa79ab2e57e4ffb812d5fb2f6cb69bdacf7aa
FerBayley <fernando.bayley@gmail.com>
2015-04-12 23:20:07 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Convivir Plus</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!-- CSS -->
+
+</head>
+<body>
+
+	<header>
+		<div class="Contenedor_header">
+			<ul>
+				<li><h1><img src="img/logo.jpg" width="50" height="50" alt=""></h1></li>
+				<li>CONVIVIR <br> <span>+PLUS</span></li>
+			</ul>
+		</div> <!-- End of Contenedor_header -->
+
+		<section class="Leyenda">
+			<p>Llega Convivir+Plus la herramienta definitiva</p>
+			<p>destinada al segmento del buen vivir</p>
+		</section> <!-- End of Leyenda -->
+
+
+		<section class="CallToAcction">
+			<a href="#">CONTACTAR</a>
+		</section> <!-- End of CallToAcction -->
+
+		<section class="Tel">
+			<p>M&aacute;s info al: 0800-888-convivirplus - <a href="#">plus@convivirpress.com</a></p>
+		</section> <!-- End of Tel -->
+	</header> <!-- End of header -->
+
+	<section class="Clientes">
+		<div class="Contenedor_Clientes">
+			<div class="Col_Clientes1">
+				<img src="img/marcas/crinway.jpg" alt="">
+			</div> <!-- End of Col_Clientes1 -->
+
+			<div class="Col_Clientes2">
+				<img src="img/marcas/dicomere.jpg" width="150" height="91" alt="">
+			</div> <!-- End of Col_Clientes2 -->
+
+			<div class="Col_Clientes3">
+				<img src="img/marcas/emeth.jpg" alt="">
+			</div> <!-- End of Col_Clientes3 -->
+
+			<div class="Col_Clientes4">
+				<img src="img/marcas/villares.jpg" alt="">
+			</div> <!-- End of Col_Clientes4 -->
+		</div> <!-- End of Contenedor_Clientes -->
+	</section> <!-- End of Clientes -->
+
+	<section class="Columas">
+		<div class="Contenedor_columnas">
+			<div class="Col1">
+				<img src="img/responsive.png" width="150" height="150" alt="">
+				<h2>Diseño y desarrollo web de alta calidad</h2>
+			</div> <!-- End of Col1 -->
+			<div class="Col2">
+				<img src="img/marketing.png" width="150" height="150" alt="">
+				<h2>Marketing Online, email + redes sociales + mediciones</h2>
+			</div> <!-- End of Col2 -->
+			<div class="Col3">
+				<img src="img/ideas.png" width="150" height="150" alt="">
+				<h2>Concepción y puesta en marcha de ideas para canales online</h2>
+			</div> <!-- End of Col3 -->
+			<div class="Col4">
+				<img src="img/data.png" width="150" height="150" alt="">
+				<h2>Analizamos el rendimiento de su negocio y/o servicio online</h2>
+			</div> <!-- End of Col4 -->
+		</div> <!-- Contenedor_columnas -->
+	</section> <!-- End of Columnas -->
+
+	<section class="Herramientas">
+		<q>La mejor herramienta para administrar todos los productos online de mi negocio</q>
+	</section> <!-- End of Herramientas -->
+
+	<section class="Nosotros">
+		<div class="Contenedor_nosotros">
+			<article>
+				<h2>¿Quienes somos y que hacemos?</h2>
+				<p>
+					<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt possimus, odit, sequi provident consectetur iusto officiis? Ratione accusamus, suscipit, ad tempora perferendis corrupti laborum dicta itaque tenetur perspiciatis porro explicabo.</span>
+					<span>Nemo officiis perspiciatis impedit eveniet quisquam fugit mollitia molestias perferendis ipsa eaque, dolore magni earum dolor labore quam quos ex possimus aliquid? Possimus et architecto id velit, a fuga quia.</span>
+					<span>Dolor similique consequatur aperiam reiciendis delectus omnis, veritatis vel illo, dicta ad deserunt doloribus sit enim, laborum. Numquam incidunt, reiciendis ratione magnam, ipsa delectus esse praesentium ullam atque sapiente expedita.</span>
+				</p>
+			</article>
+		</div>
+	</section> <!-- End of Nosotros -->
+
+	<section class="Servicios">
+		<article>
+			<h2>Servicios</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, quaerat tempora. Beatae possimus maxime, vitae, impedit iusto praesentium ipsum est sit accusamus esse ipsam tenetur temporibus tempora, accusantium vel totam.
+			</p>
+		</article>
+		<div class="Contenedor_Servicios1">
+			<ul>
+				<li></li>
+			</ul>
+		</div> <!-- End of Contenedor_Servicios1 -->
+
+		<div class="Contenedor_Servicios2">
+			
+		</div> <!-- End of Contenedor_Servicios2 -->
+	</section> <!-- End of Servicios -->
+	
+</body>
+</html>
